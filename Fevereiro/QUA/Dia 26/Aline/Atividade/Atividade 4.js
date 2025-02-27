f87f4c844a3f7c7bc7b0b6f8bee2a70384697b4e
Caua Bispo <cauarecuperar@gmail.com>
2025-02-26 22:29:26 -0300

    aula JS if e else

---
+++ Fevereiro/QUA/Dia 26/Aline/Atividade/Atividade 4.js
@@ -0,0 +1,6 @@
+var idade = 16;
+if(idade>=18){
+    console.log("Voce é maior de idade")
+}else{
+    console.log("Você é menor de idade")
+};
---
+++ Fevereiro/QUA/Dia 26/Aline/Atividade/Atividade 5.js
@@ -0,0 +1,14 @@
+//eu quero um programa em que a pessoa fale a idade dela e quero que a idade dela seja opcional,não pode ou obrigatorio
+var idade1 = 16;
+var idade2 = 18;
+var idade3 = 1;
+var idade4 = 70;
+if(idade2>=18<70){
+    console.log("Você é obrigado a votar")
+}if(idade1<18){
+    console.log("Você não pode votar")
+}if(idade3>=17){
+    console.log("Não é necessario você votar mas você pode")
+}if(idade4>=70){
+    console.log("Não é necessario você votar mas você pode")
+}
